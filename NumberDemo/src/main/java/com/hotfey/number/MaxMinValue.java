64d2f8ff18837a3ab9da57d0650966b798faa44f
hotfey <hotfey@foxmail.com>
2016-01-12 11:32:06 +0800

    MaxMinValue

---
+++ NumberDemo/src/main/java/com/hotfey/number/MaxMinValue.java
@@ -0,0 +1,36 @@
+package com.hotfey.number;
+
+public class MaxMinValue {
+	public static byte maxByte() {
+		return Byte.MAX_VALUE;
+	}
+
+	public static byte minByte() {
+		return Byte.MIN_VALUE;
+	}
+
+	public static short maxShort() {
+		return Short.MAX_VALUE;
+	}
+
+	public static short minStort() {
+		return Short.MIN_VALUE;
+	}
+
+	public static int maxInt() {
+		return Integer.MAX_VALUE;
+	}
+
+	public static int minInt() {
+		return Integer.MIN_VALUE;
+	}
+
+	public static long MaxLong() {
+		return Long.MAX_VALUE;
+	}
+
+	public static long minLong() {
+		return Long.MIN_VALUE;
+	}
+
+}
---
+++ NumberDemo/src/test/java/com/hotfey/number/MaxMinValueTest.java
@@ -0,0 +1,20 @@
+package com.hotfey.number;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MaxMinValueTest {
+	@Ignore
+	@Test
+	public void testMaxInt() {
+		System.out.println(MaxMinValue.maxInt());
+		System.out.println(String.valueOf(MaxMinValue.maxInt()).length());
+	}
+
+	@Ignore
+	@Test
+	public void testMinInt() {
+		System.out.println(MaxMinValue.minInt());
+		System.out.println(String.valueOf(MaxMinValue.minInt()).length());
+	}
+}
